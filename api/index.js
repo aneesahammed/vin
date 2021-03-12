6454e7425c4506af38752913898edf72b91c0003
@aneesahammed <aneesahammed@gmail.com>
2021-03-12 23:16:40 +0000

    adds splitbee

--- api/index.js
+++ api/index.js
@@ -1,8 +1,15 @@
+import splitbee from "@splitbee/web";
 import { validateNif } from "../lib";
 
 module.exports = (req, res) => {
+  splitbee.init();
+
   if (req.method === "GET") {
     const { q } = req.query;
+
+    splitbee.track("Query", {
+      query: q,
+    });
 
     if (q) {
       let obj = {};
@@ -11,14 +18,21 @@ module.exports = (req, res) => {
     } else return res.status(200).json("Hello World!");
   } else if (req.method === "POST") {
     let data = req.body;
-    data = data.data;
+    data = data?.data;
 
-    let obj = {};
-    data.forEach((item) => {
-      obj[item] = validateNif(item);
-    });
+    if (data) {
+      console.log("data->" + data);
+      splitbee.track("POST", {
+        data: data,
+      });
 
-    return res.status(200).json(obj);
+      let obj = {};
+      data.forEach((item) => {
+        obj[item] = validateNif(item);
+      });
+
+      return res.status(200).json(obj);
+    } else return res.status(200).json("Hello World!");
   } else {
     return res.status(405).json({
       Error: "Method not allowed",
